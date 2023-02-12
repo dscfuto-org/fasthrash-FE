2b2bf12e2a1eaf99c1294a2abb7ad2ddd062b9f7
theknowledgepro <ajibopromise10@gmail.com>
2023-02-12 20:09:46 +0100

    Created the ErrorPage and made some necessary changes to App.js file too!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import Rootlayout from "./pages/Rootlayout";
 import Welcome from "./pages/Welcome";
 import Login from "./pages/login";
 import Signup from "./pages/Signup";
+import ErrorPage from './pages/ErrorPage';
+
+// This can be imported by anyone throughout the app instead of having to declare the color or site name in all files...
+// For example, I imported it in the ErrorPage.js file
+// And to change the color, we only have to change this constant value here...
+export const THEME_COLOR = '#7F56D9'; 
+export const SITE_NAME = 'FAST TRASH'
+
 const route = createBrowserRouter([
   {
     path: "/",
@@ -12,6 +20,10 @@ const route = createBrowserRouter([
         index: true,
         path: "/",
         element: <Welcome />,
+      },
+      {
+        path: "*",
+        element: <ErrorPage />, //This is the page for all 404 routes on this website!
       },
       {
         path: "login",
