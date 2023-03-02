ea65e20392d8794cef02921067a5c92b845ebe91
Codeindeed <favouralex084@gmail.com>
2023-03-02 21:57:47 +0100

    made some changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
-import { ContextProvider } from './context';
+import { ContextProvider } from './context/index';
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
