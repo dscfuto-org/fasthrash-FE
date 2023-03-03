ae083afc256d2ab80b0b0648bc450fb9d57205c5
theknowledgepro <ajibopromise10@gmail.com>
2023-03-03 08:00:58 +0100

    Uncommented the comment on dashboard util for token checks

--- src/util/dashboard.js
+++ src/util/dashboard.js
@@ -1,10 +1,10 @@
 import { redirect } from "react-router-dom";
 
 export async function loader({ request, params }) {
-  // const token = localStorage.getItem("token");
-  // if (!token) {
-  //   return redirect("/login");
-  // }
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return redirect("/login");
+  }
 
   return null;
 }
